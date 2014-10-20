8c1767495f358c4ae2226ab799fe0cd8f6f3bbfd
Rob McMorran <Rob@Robs-MacBook-Air.local>
2014-10-20 14:03:44 +0100

    Initial Commit

---
+++ RestTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RestTest
+//
+//  Created by Rob McMorran on 20/10/2014.
+//  Copyright (c) 2014 Rob McMorran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RestTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RestTest
+//
+//  Created by Rob McMorran on 20/10/2014.
+//  Copyright (c) 2014 Rob McMorran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
